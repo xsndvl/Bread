e49981cfa1b5e020747a32f750a58baa76fc573a
Joswag Sandoval <sergiojr100@outlook.com>
2021-12-20 18:49:02 -0600

    Just saving progress so far

--- controllers/breads_controllers.js
+++ controllers/breads_controllers.js
@@ -3,11 +3,17 @@ const breads = express.Router()
 const Bread = require("../models/bread")
 
 breads.get("/", (req, res) =>{
-    res.send(Bread)
+    // res.send(Bread)
+    res.render("Index", {
+        breads: Bread
+    })
 })
 
 breads.get("/:arrayIndex", (req, res) =>{
-    res.send(Bread[req.params.arrayIndex])
+    // res.send(Bread[req.params.arrayIndex])
+    res.render("Show", {
+        bread: Bread[req.params.arrayIndex]
+    })
 })
 
 module.exports = breads
--- index.js
+++ index.js
@@ -8,6 +8,12 @@ const PORT = process.env.PORT
 const app = express()
 console.log(PORT)
 
+//MIDDLE WEAR
+app.use(express.static("public"))
+app.set('views', __dirname + '/views')
+app.set("view engine", "jsx")
+app.engine('jsx', require('express-react-views').createEngine())
+
 //ROUTES
 app.get('/', (req, res) =>{
     res.send('Welcome to an Awesome App about Breads!')
@@ -17,5 +23,5 @@ app.use("/breads", breadsController)
 
 //Listen
 app.listen(PORT, () =>{
-    console.log('nomming at port', PORT)
+    console.log('listening at port', PORT)
 })
